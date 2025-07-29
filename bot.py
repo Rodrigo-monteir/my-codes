43ff23eb39eb335ab05507394466dd325aec8c6b
Rodrigo Monteiro <rodrigo.f.monteiro6@gmail.com>
2025-07-29 21:52:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,21 +3,26 @@ import keyboard
 import time
 from deep_translator import GoogleTranslator
 
-print("Pressione Ctrl + C para copiar texto.")
-print("Pressione Ctrl + T para traduzir o texto copiado.")
+while True:
+    print("Pressione Ctrl + C para copiar texto.")
+    print("Pressione Ctrl + T para traduzir o texto copiado.")
 
-keyboard.wait('ctrl+t')
-time.sleep(0.5)
-print("Copiando texto...")
-texto = pyperclip.paste()
-print("Texto copiado:")
-if not texto.strip():
-    print("Nenhum texto copiado.")
-    exit()
-print("tradução iniciada...")
-traduzido = GoogleTranslator(source='auto', target='pt').translate(texto)
-print("Espere 2 segundos...")    
-time.sleep(2)
-print("Texto traduzido:", traduzido)
-pyperclip.copy(traduzido)
-print("Texto traduzido copiado para a área de transferência.")
+    keyboard.wait('ctrl+t')
+    time.sleep(0.5)
+
+    print("Copiando texto...")
+    texto = pyperclip.paste()
+    print("Texto copiado:")
+
+    if not texto.strip():
+        print("Nenhum texto copiado.")
+        exit()
+
+    print("tradução iniciada...")
+    traduzido = GoogleTranslator(source='auto', target='pt').translate(texto)
+    print("Espere 2 segundos...")    
+    time.sleep(2)
+    
+    print("Texto traduzido:", traduzido)
+    pyperclip.copy(traduzido)
+    print("Texto traduzido copiado para a área de transferência.")
