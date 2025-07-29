63e82241a9079b2bc38d1f9736677c99220c6775
Rodrigo Monteiro <rodrigo.f.monteiro6@gmail.com>
2025-07-29 21:51:14 +0100

    Update bot_1.py

--- bot_1.py
+++ bot_1.py
@@ -3,41 +3,38 @@ import keyboard
 import time
 from deep_translator import GoogleTranslator
 
-lingua = input("Digite a língua do texto para que pretende traduzir(ex: ingles, portugues):")
+while True:
+    lingua = input("Digite a língua do texto para que pretende traduzir(ex: ingles, portugues):")
 
-linguas = ['en', 'pt', 'es', 'fr', 'de', 'it']
-linguas_1 = ['ingles', 'portugues', 'espanhol', 'frances', 'alemao', 'italiano']
+    linguas = ['en', 'pt', 'es', 'fr', 'de', 'it']
+    linguas_1 = ['ingles', 'portugues', 'espanhol', 'frances', 'alemao', 'italiano']
 
-for i in range(len(linguas_1)):
-    if lingua.lower() == linguas_1[i]:
-        lingua = linguas[i]
-        break
+    for i in range(len(linguas_1)):
+        if lingua.lower() == linguas_1[i]:
+            lingua = linguas[i]
+            break
 
-bit = 0
-if bit == 0:
     texto = input("Meta o texto que pretento transformar.")
-    bit = 1
 
-if bit == 1:
     print("Pressione Ctrl + T para iniciar o processo.")
 
-keyboard.wait('ctrl+t')
-time.sleep(0.5)
-print("Copiando texto...")
+    keyboard.wait('ctrl+t')
+    time.sleep(0.5)
+    print("Copiando texto...")
 
 
-print("Texto copiado")
+    print("Texto copiado")
 
-if not texto.strip():
-    print("Nenhum texto detectado.")
-    exit()
+    if not texto.strip():
+        print("Nenhum texto detectado.")
+        exit()
 
-print("tradução iniciada...")
-traduzido = GoogleTranslator(source='auto', target=lingua).translate(texto)
-print("Espere 2 segundos...")    
-time.sleep(2)
+    print("tradução iniciada...")
+    traduzido = GoogleTranslator(source='auto', target=lingua).translate(texto)
+    print("Espere 2 segundos...")    
+    time.sleep(2)
 
-print("Texto traduzido:", traduzido)
+    print("Texto traduzido:", traduzido)
 
-pyperclip.copy(traduzido)
-print("Texto traduzido copiado para a área de transferência.")
+    pyperclip.copy(traduzido)
+    print("Texto traduzido copiado para a área de transferência.")
